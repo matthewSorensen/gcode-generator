779de4697cbb2cf15295bd3727af294ad13b19cf
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-10-30 22:29:47 -0700

    bug in shapely with intersections of LinearRings? I think so

--- cell.py
+++ cell.py
@@ -1,8 +1,9 @@
 from shapely.geometry import LineString, MultiLineString, LinearRing, Polygon
-
+from shapely.ops import linemerge
 from math import sqrt
 import numpy as np
 from numpy.linalg import norm
+import shapely_utils
 import visualize
 
 def extend(tip, first, length):
@@ -13,48 +14,50 @@ def extend(tip, first, length):
     tip = first + (length / norm(direction)) * direction
     return (tip[0], tip[1])
 
+
 def extend_line(line, length):
+
     points = list(line.coords)
     points[0] = extend(points[0],points[1],length)
     points[-1] = extend(points[-1],points[-2],length)
 
     return points
+i = 0
 
 def to_visual(gen):
+    global i
     for seg in gen:
-        yield list((p[0],p[1], 0)       for p in seg.coords)
+        i -= 1
+        yield list((p[0],p[1], i) for p in seg.coords)
+
+def dist(a,b):
+    return sqrt(pow(a[0]-b[0],2)+ pow(a[1]-b[1],2))
 
 
-def arbitrary_bounding_box(start,end, coords, big):
-    start = np.array(start)
-    end = np.array(end)
+
+def new_bound(linear_ring, line, big):    
+
+    start = np.array(line.coords[0])
+    end = np.array(line.coords[-1])
 
     ihat = end - start
     ihat = ihat * 1 / norm(ihat)
     jhat = np.array([-1 * ihat[1], ihat[0]]) # ccw 90 degree rotation, defining the rest of our coordinate system
 
-    xbounds, ybounds = (0,0),(0,0)
+    low = start - big * ihat
+    high = start + big * ihat
 
-    for point in coords:
-        disp = np.array(point) - start
-        xc = np.dot(ihat,disp)
-        yc = np.dot(jhat,disp)
-        
-        xbounds = (min(xbounds[0], xc), max(xbounds[1], xc))
-        ybounds = (min(ybounds[0], yc), max(ybounds[1], yc))
- 
-    low = start + xbounds[0] * ihat
-    high = end + max(0, xbounds[1] - np.dot(ihat, end)) * ihat
-    lower = start + (xbounds[0] - big) * ihat
-    higher = end + max(0, big + xbounds[1] - np.dot(ihat, end)) * ihat
-    
-#    box = [lower, low, low - big * jhat, high - big * jhat, high, higher, higher + big * jhat, lower + big * jhat]
-#    box = [ lower, low, low - big * jhat, high - big * jhat, high + big * jhat, low + big* jhat]
+    box = Polygon([low, high, high + big * jhat, low + big * jhat]) # half-plane!
+    new = box.intersection(linear_ring)
 
+    if type(new) == MultiLineString:
+        return shapely_utils.best_closure(line, new)
 
-    # meh, we need to partition all of the region into non-intersecting subregions - don't need complete coverage, but
-    # we do need complete coverage of the areas we haven't covered.
-    return box
+    visualize.show_paths(to_visual([line]))
+    visualize.show_paths(to_visual([linear_ring]))
+
+    return shapely_utils.best_closure(line, [new])
+
 
 class Cell:
 
@@ -70,38 +73,35 @@ class Cell:
     def construct(self):
         last = self.start        
 
-
         bounds = self.region.bounds
         size = sqrt(pow(bounds[2]-bounds[0],2)+pow(bounds[3]-bounds[1],2))
 
+        region = Polygon(self.region.coords)
+        if not region.is_valid:
+            return
         while True:
             self.paths.append(last)
             last = self.offset(last,size)
-
             if last.is_empty:
                 return
 
-            inside = self.region.intersection(last)
-          
+            inside = region.intersection(last)          
             if inside.is_empty:
                 return
 
+            if type(inside) == MultiLineString and len(inside) == 2 and type(last) == LinearRing:
+                inside = linemerge(inside)
+
             if type(inside) == MultiLineString:
                 for line in inside:
+                        p = new_bound(self.region, line, size)
+                        if p.is_empty:
+                            continue
 
-                    p = arbitrary_bounding_box(line.coords[0],line.coords[-1],line.coords, 100)
-
-                 #   visualize.show_paths(to_visual([LinearRing(p)]))
-                    
-                    box = Polygon(p)
-
-                    region = self.region.intersection(box) 
-                    if region.is_empty:
-                        continue
-
-                    sub = Cell(region,box.intersection(self.bound),line, self.offset)
-                    sub.construct()
-                    self.subregions.append(sub)
+                        sub = Cell(p, None, line, self.offset)
+        
+                        sub.construct()
+                        self.subregions.append(sub)
                 return
 
             last = inside
@@ -116,13 +116,12 @@ class Cell:
             yield seg
 
 def offsetting(obj,size):
-    off = obj.parallel_offset(1,'left', join_style = 2)
+    off = obj.parallel_offset(2,'left', join_style = 3)
     if off.is_empty:
         return off
 
-
-    if type(off) == LinearRing:
-        return off
+    if type(obj) == LinearRing and type(off) == LineString:
+        return shapely_utils.closed_linear_ring(list(off.coords))
 
     # take the first and last line segments, and make them very long
     copy = list(off.coords)
